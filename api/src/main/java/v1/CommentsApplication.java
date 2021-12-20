f184c10b2a3eeea937bddc1ed293b646deab9b8c
janezbozic <janez.bozic1@gmail.com>
2021-12-20 21:00:30 +0100

    Added OpenApi

--- api/src/main/java/v1/CommentsApplication.java
+++ api/src/main/java/v1/CommentsApplication.java
@@ -1,10 +1,20 @@
 package v1;
 
+import org.eclipse.microprofile.openapi.annotations.OpenAPIDefinition;
+import org.eclipse.microprofile.openapi.annotations.info.Contact;
+import org.eclipse.microprofile.openapi.annotations.info.Info;
+import org.eclipse.microprofile.openapi.annotations.info.License;
+import org.eclipse.microprofile.openapi.annotations.servers.Server;
+
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.GET;
 import javax.ws.rs.core.Application;
 import javax.ws.rs.core.Response;
 
+@OpenAPIDefinition(info = @Info(title = "Badminton app comments API", version = "v1",
+        contact = @Contact(email = "rso@fri.uni-lj.si"),
+        license = @License(name = "dev"), description = "API for Badminton app comments."),
+        servers = @Server(url = "http://20.62.136.0:8080/")) //TO-DO
 @ApplicationPath("/v1")
 public class CommentsApplication extends Application {
 
